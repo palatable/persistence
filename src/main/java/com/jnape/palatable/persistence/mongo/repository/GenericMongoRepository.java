c46aa7b6d4e42572e72d0cb00dd6a71618f39064
jnape <jnape09@gmail.com>
2014-04-21 00:05:07 -0500

    Adding error handling for type parameter reification limitations on GenericMongoRepository, and tightening up method visibility

--- src/main/java/com/jnape/palatable/persistence/mongo/repository/GenericMongoRepository.java
+++ src/main/java/com/jnape/palatable/persistence/mongo/repository/GenericMongoRepository.java
@@ -9,6 +9,7 @@ import com.mongodb.util.JSON;
 import org.bson.types.ObjectId;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Optional;
 
 import static com.jnape.palatable.persistence.Entity.entity;
@@ -16,48 +17,68 @@ import static java.util.stream.StreamSupport.stream;
 
 public abstract class GenericMongoRepository<Payload> implements MongoRepository<Payload, String> {
 
+    private final Class<Payload>                       payloadClass;
     private final DBCollection                         collection;
     private final BidirectionalJsonSerializer<Payload> serializer;
 
-    public GenericMongoRepository(DBCollection collection, BidirectionalJsonSerializer<Payload> serializer) {
+    public GenericMongoRepository(Class<Payload> payloadClass,
+                                  DBCollection collection,
+                                  BidirectionalJsonSerializer<Payload> serializer) {
+        this.payloadClass = payloadClass;
         this.collection = collection;
         this.serializer = serializer;
     }
 
+    public GenericMongoRepository(DBCollection collection, BidirectionalJsonSerializer<Payload> serializer) {
+        this.collection = collection;
+        this.serializer = serializer;
+        payloadClass = derivePayloadClass();
+    }
+
     @Override
-    public Optional<Entity<Payload, String>> findOne(DBObject query) {
+    public final Optional<Entity<Payload, String>> findOne(DBObject query) {
         DBObject record = collection.findOne(query);
         return Optional.ofNullable(record).map(this::toEntity);
     }
 
     @Override
-    public Iterable<Entity<Payload, String>> find(DBObject query) {
+    public final Iterable<Entity<Payload, String>> find(DBObject query) {
         DBCursor dbObjects = collection.find(query);
         return () -> stream(dbObjects.spliterator(), false).map(this::toEntity).iterator();
     }
 
-    private Entity<Payload, String> toEntity(DBObject dbObject) {
-        Payload payload = serializer.deserialize(payloadClass(), dbObject.toString());
-        return entity(payload, dbObject.get("_id").toString());
-    }
-
     @Override
-    public Entity<Payload, String> save(Payload payload) {
+    public final Entity<Payload, String> save(Payload payload) {
         DBObject dbObject = (DBObject) JSON.parse(serializer.serialize(payload));
         collection.save(dbObject);
         return entity(payload, dbObject.get("_id").toString());
     }
 
     @Override
-    public Entity<Payload, String> save(Entity<Payload, String> entity) {
+    public final Entity<Payload, String> save(Entity<Payload, String> entity) {
         DBObject dbObject = (DBObject) JSON.parse(serializer.serialize(entity.get()));
         dbObject.put("_id", new ObjectId(entity.id()));
         collection.save(dbObject);
         return entity;
     }
 
+    protected final Entity<Payload, String> toEntity(DBObject dbObject) {
+        Payload payload = serializer.deserialize(payloadClass, dbObject.toString());
+        return entity(payload, dbObject.get("_id").toString());
+    }
+
     @SuppressWarnings("unchecked")
-    protected Class<Payload> payloadClass() {
-        return (Class<Payload>) ((ParameterizedType) (getClass().getGenericSuperclass())).getActualTypeArguments()[0];
+    private Class<Payload> derivePayloadClass() {
+        Type payloadType = ((ParameterizedType) (getClass().getGenericSuperclass())).getActualTypeArguments()[0];
+        if (payloadType instanceof Class)
+            return (Class<Payload>) payloadType;
+
+        throw new IllegalStateException(
+                "Could not derive concrete class from Payload type due to limitations with Java Generics; consider either:" +
+                        "\n" +
+                        "\t1) Calling the constructor with the concrete Payload class specified, or" +
+                        "\n" +
+                        "\t2) Substituting a concrete type for the Payload type argument in your Repository subclass"
+        );
     }
 }
